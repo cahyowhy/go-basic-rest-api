d43f59ccc4483d4c1f949c884b0754af6af04da4
cahyowhy <cahyowhy01@gmail.com>
2018-10-24 12:20:14 +0700

    fixing env

--- main.go
+++ main.go
@@ -9,15 +9,11 @@ import (
 
 func main() {
 	var env = flag.String("env", "DEV", "type environment")
-	var portrun string
-
 	flag.Parse()
 
 	if *env == "PROD" {
-		portrun = ":80"
 		godotenv.Load(".env.production")
 	} else {
-		portrun = ":3000"
 		godotenv.Load(".env.development")
 	}
 
@@ -30,5 +26,5 @@ func main() {
 		app.seedsDb()
 	}
 
-	app.Run(portrun)
+	app.Run(":3000")
 }
