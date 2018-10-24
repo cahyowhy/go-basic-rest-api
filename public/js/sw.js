2b8f035d2b3d438b2b51c1b625709605fc81322d
cahyowhy <cahyowhy01@gmail.com>
2018-10-24 11:02:53 +0700

    adding some file to deploy

---
+++ public/js/sw.js
@@ -0,0 +1,57 @@
+/*
+ *  Kita akan ubah nama
+ *  pada cacheVersion
+ *  supaya browser tahu bahwa
+ *  ada perubahan terbaru
+ */
+const cacheVersion = 'todo-app';
+const filesToCache = [
+    '/',
+    '/public/css/app.css',
+    '/public/js/app.js',
+    '/public/vendor/js/jquery.migrate.min.js',
+    '/public/vendor/js/jquery.min.js',
+];
+
+self.addEventListener('install', function (event) {
+    console.log('start fetch');
+    event.waitUntil(
+        caches.open(cacheVersion)
+            .then(function (cache) {
+                return cache.addAll(filesToCache)
+            })
+    )
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (res) {
+                if (res) return res;
+
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames
+                    .filter(function (cacheName) {
+                        return cacheName !== cacheVersion;
+                    })
+                    .map(function (cacheName) {
+                        caches.delete(cacheName);
+                    })
+            );
+        })
+    );
+});
+
+self.addEventListener('message', function (event) {
+    if (event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
